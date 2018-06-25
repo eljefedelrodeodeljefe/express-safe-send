c49febfd4b7f0762a9285276514fadfc5a66324c
Robert Lindstaedt <robert.lindstaedt@gmail.com>
2018-06-25 12:01:24 +0200

    fix(main): add missing args from docs

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ module.exports = (options = {}) => {
 
   return function safeSendMiddleware (req, res, next) {
     res.safeSend = function safeSend (body, cb) {
-      if (res.headersSent && typeof cb === 'function') return cb(new Error('express-safe-send: headers were already are sent'))
+      if (res.headersSent && typeof cb === 'function') return cb(new Error('express-safe-send: headers were already are sent'), req, res, next)
       if (res.headersSent) return
 
       res.send(body)
 
-      if (typeof cb === 'function') return cb()
+      if (typeof cb === 'function') return cb(null, req, res, next)
     }
 
     res.safeJsonSend = function safeSend (body, cb) {
-      if (res.headersSent && typeof cb === 'function') return cb(new Error('express-safe-send: headers were already are sent'))
+      if (res.headersSent && typeof cb === 'function') return cb(new Error('express-safe-send: headers were already are sent'), req, res, next)
       if (res.headersSent) return
 
       res.json(body)
 
-      if (typeof cb === 'function') return cb()
+      if (typeof cb === 'function') return cb(null, req, res, next)
     }
 
     return next()
